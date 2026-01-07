7b062b0d94117ad755a1d12a8b5fa4d781d0495a
Tuanno <tuannodanyllo26@gmail.com>
2026-01-07 12:01:32 -0300

    criado configurações de rota no Sidebar

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -4,7 +4,11 @@ import { Link } from '@inertiajs/react';
 export default function Sidebar() {
   return (
     <div className="h-screen w-48 bg-green-600 p-4 flex flex-col gap-4">
-      <h1 className="text-white font-bold text-lg mb-6">CONECTAEBD</h1>
+      <img
+  src="/imagens/logo.png"
+  alt="Logo CONECTAEBD"
+  className="w-auto h-30 mb-6" // Ajuste as classes de tamanho (ex: h-10 para 40px de altura)
+/>
 
       <button className="flex items-center gap-2 p-2 bg-white text-green-600 rounded hover:bg-green-100">
         {/* Link para Professores */}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rotas para buscar alunos por classe
+    Route::get('/api/students/{classGroup}', [StudentController::class, 'getStudentsByClass']);
+    Route::get('/api/students', [StudentController::class, 'getAllStudents']);
 });
 
 require __DIR__.'/auth.php';
